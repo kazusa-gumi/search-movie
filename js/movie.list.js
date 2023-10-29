d5d7dde03109db2e75b737a62e3246217f955c1a
kazusa-gumi <jsva4127@yahoo.co.jp>
2023-10-30 10:52:33 +1100

    追加

--- js/movie.list.js
+++ js/movie.list.js
@@ -21,4 +21,54 @@ class MovieList {
 
         return html
     }
+
+    refresh() {
+        let elementString = this.movieItemString(this.MovieList);
+        let rootElement = document.getElementById(this.rootId);
+        rootElement.innerHTML = elementString;
+    }
+
+    // Add a new Movie
+    add(title, year) {
+        // add a new item to the end of the array
+        this.MovieList.push({ title: title, year: year });
+        // this.MovieList.push(title, year); this came in, e6 Js.
+        // Refresh the view
+        this.refresh();
+    }
+
+    // Update a movie by index
+    update(index, title, year) {
+        this.MovieList[index].title = title;
+        this.MovieList[index].year = year;
+        this.refresh();
+    }
+    // Delete a movie ny index
+    delete(index) {
+        this.MovieList.splice(index, 1);
+        this.refresh();
+    }
+    // Sort by Title A - Z
+    sortAZZ() {
+        this.MovieList.sort(function (a, b) {
+            return a.title.localeCompare(b.title);
+        })
+        this.refresh();
+    }
+    // Sort by Title Z - A
+    sortAZZ() {
+        this.MovieList.sort(function (a, b) {
+            return b.title.localeCompare(a.title);
+        })
+        this.refresh();
+    }
+    // Search by partial title
+    search(nameString) {
+        let shortList = [];
+        for (let movie of this.MovieList) {
+            if (movie.title.includes(nameString)) {
+                shortList.push(movie)
+            }
+        }
+    }
 }
